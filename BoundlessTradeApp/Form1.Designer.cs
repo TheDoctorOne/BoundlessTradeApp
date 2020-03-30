ff2668f83627f3b028eeafcdb93a54b99c5399cb
TheDoctorOne <mahmudkocas@gmail.com>
2020-03-30 13:58:32 +0300

    Workin Software

--- BoundlessTradeApp/Form1.Designer.cs
+++ BoundlessTradeApp/Form1.Designer.cs
@@ -28,13 +28,184 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.itemListBox = new System.Windows.Forms.ListBox();
+            this.buyListBox = new System.Windows.Forms.ListBox();
+            this.sellListBox = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label4 = new System.Windows.Forms.Label();
+            this.ITEM = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PROFIT = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.theyBuy = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.theySell = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label";
+            // 
+            // itemListBox
+            // 
+            this.itemListBox.FormattingEnabled = true;
+            this.itemListBox.Location = new System.Drawing.Point(667, 26);
+            this.itemListBox.Name = "itemListBox";
+            this.itemListBox.Size = new System.Drawing.Size(364, 407);
+            this.itemListBox.TabIndex = 1;
+            this.itemListBox.Visible = false;
+            this.itemListBox.SelectedIndexChanged += new System.EventHandler(this.itemListBox_SelectedIndexChanged);
+            // 
+            // buyListBox
+            // 
+            this.buyListBox.FormattingEnabled = true;
+            this.buyListBox.Location = new System.Drawing.Point(6, 40);
+            this.buyListBox.Name = "buyListBox";
+            this.buyListBox.Size = new System.Drawing.Size(155, 199);
+            this.buyListBox.TabIndex = 2;
+            // 
+            // sellListBox
+            // 
+            this.sellListBox.FormattingEnabled = true;
+            this.sellListBox.Location = new System.Drawing.Point(167, 40);
+            this.sellListBox.Name = "sellListBox";
+            this.sellListBox.Size = new System.Drawing.Size(155, 199);
+            this.sellListBox.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.buyListBox);
+            this.groupBox1.Controls.Add(this.sellListBox);
+            this.groupBox1.Location = new System.Drawing.Point(1047, 77);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(331, 251);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Select Item from Left Box";
+            this.groupBox1.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(216, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Shop Stand";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Request Basket";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ITEM,
+            this.PROFIT,
+            this.theyBuy,
+            this.theySell});
+            this.dataGridView1.Location = new System.Drawing.Point(15, 26);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(1026, 725);
+            this.dataGridView1.TabIndex = 5;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(938, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Click Twice to Item";
+            // 
+            // ITEM
+            // 
+            this.ITEM.HeaderText = "ITEM";
+            this.ITEM.Name = "ITEM";
+            this.ITEM.ReadOnly = true;
+            this.ITEM.Width = 200;
+            // 
+            // PROFIT
+            // 
+            this.PROFIT.HeaderText = "PROFIT";
+            this.PROFIT.Name = "PROFIT";
+            this.PROFIT.ReadOnly = true;
+            this.PROFIT.Width = 50;
+            // 
+            // theyBuy
+            // 
+            this.theyBuy.HeaderText = "THEY SELL";
+            this.theyBuy.Name = "theyBuy";
+            this.theyBuy.ReadOnly = true;
+            this.theyBuy.Width = 250;
+            // 
+            // theySell
+            // 
+            this.theySell.HeaderText = "THEY BUY";
+            this.theySell.Name = "theySell";
+            this.theySell.ReadOnly = true;
+            this.theySell.Width = 250;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1052, 763);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.itemListBox);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Boundless Easy Money";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox itemListBox;
+        private System.Windows.Forms.ListBox buyListBox;
+        private System.Windows.Forms.ListBox sellListBox;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ITEM;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PROFIT;
+        private System.Windows.Forms.DataGridViewTextBoxColumn theyBuy;
+        private System.Windows.Forms.DataGridViewTextBoxColumn theySell;
     }
 }
 
--- BoundlessTradeApp/Form1.cs
+++ BoundlessTradeApp/Form1.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +15,296 @@ namespace BoundlessTradeApp
 {
     public partial class Form1 : Form
     {
+        private ArrayList itemEnglish;
+        private ArrayList itemList;
+        private ArrayList planetList;
+        private String API_URL_BASE = "https://butt.boundless.mayumi.fi/api/item/";
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            String Planet_URL = "https://butt.boundless.mayumi.fi/api/planets/";
+            String Item_URL = "https://butt.boundless.mayumi.fi/api/itemlist/";
+            String English_URL = "https://butt.boundless.mayumi.fi/gameFiles/raw/english.json";
+
+            itemEnglish = readEnglish(getData(English_URL));
+            //String url = "https://butt.boundless.mayumi.fi/api/item/COAL_DEFAULT_COMPACT";
+            //ArrayList l = readTradeValues(getData(url), "COAL_DEFAULT_COMPACT");
+            //itemListBox.Items.Add(l[0]);
+            dataGridView1.Rows.Clear();
+            new Thread(delegate ()
+            {
+                itemList = readItemList(getData(Item_URL));
+            }).Start();
+
+
+
+            /*
+            foreach(String[] tmp in itemEnglish)
+            {
+                itemListBox.Items.Add(tmp[1]);
+            }*/
+        }
+        private bool firstAdd = true;
+        private void invoke(float profit, String[] toAdd, String name)
+        {
+            if (InvokeRequired)
+            {
+                this.Invoke(new Action<float, string[], string>(invoke), new object[] { profit, toAdd, name });
+                return;
+            }
+            if (!toAdd.Equals(""))
+            {
+                if(firstAdd)
+                {
+                    firstAdd = false;
+                    dataGridView1.Rows[0].SetValues(name, profit, toAdd[0], toAdd[1]);
+                    label1.Text = "Fetching : " + name;
+                    return;
+                }
+                dataGridView1.Rows.Add(name, profit, toAdd[0], toAdd[1]);
+                //itemListBox.Items.Add(toAdd);
+            }
+            
+            label1.Text = name;
+            /*
+            foreach (ArrayList tmp in itemList)
+            {
+                String name = (String)tmp[0];
+                String profit = (String)((ArrayList)tmp[1])[0];
+                itemListBox.Items.Add(name + " " + profit);
+            }*/
+        }
+
+        private String getData(String url)
+        {
+            string s;
+            using (WebClient client = new WebClient())
+            {
+                s = client.DownloadString(url);
+            }
+            return s;
+        }
+
+        private ArrayList readEnglish(String data)
+        {
+            /*
+             * 0 : StringId
+             * 1 : English Name of the Item
+             */
+            ArrayList result = new ArrayList();
+            String[] array = data.Split('\n');
+            foreach (String tmp in array)
+            {
+                if(tmp.Contains(":"))
+                {
+                    String[] temp = tmp.Split(':');
+                    String[] toAdd = { temp[0].Trim().Trim('"'), temp[1].Trim().Trim(',').Trim('"') };
+                    result.Add(toAdd);
+                }
+            }
+            
+            return result;
+        }
+
+        private ArrayList readTradeValues(String data, String temporary)
+        {
+            ArrayList result = new ArrayList();
+            ArrayList buyList = new ArrayList();
+            ArrayList sellList = new ArrayList();
+            String[] array = data.Split('\n');
+            /*
+             * 0 : price
+             * 1 : quantity
+             * 2 : guild
+             * 3 : beacon
+             * 4 : planet id
+             */
+            bool buy = true;
+            bool first_cheapest = true;
+            bool first_expensive = true;
+            String cheapest_String = "";
+            String expensive_String = "";
+            String c_qty = "";
+            String e_qty = "";
+            float cheapest = 0;
+            float expensive = 0;
+            for (int i = 0; i < array.Length;)
+            {
+                String tmp = array[i];
+                //Console.WriteLine(tmp);
+                if (tmp.Contains("sell"))
+                    buy = false;
+                if (tmp.Contains("\"buy\":"))
+                    buy = true;
+                if (tmp.Contains("price"))
+                {
+                    String price    = array[i].Split(':')[1].Trim().Trim(',').Trim();
+                    String quantity = array[i+1].Split(':')[1].Trim().Trim(',').Trim();
+                    String guild    = array[i+2].Split(':')[1].Trim().Trim(',').Trim('"').Trim();
+                    String beacon   = array[i+3].Split(':')[1].Trim().Trim(',').Trim('"').Trim();
+                    String planet   = array[i+4].Split(':')[1].Trim();
+                    //Console.WriteLine(temporary + ":" + buy + ":" + price + ":" + quantity + ":" + guild + ":" + beacon + ":" + planet);
+
+                    String[] toAdd = { price, quantity, guild, beacon, planet };
+
+                    if(buy)
+                    {
+                        if (cheapest < float.Parse(price) && float.Parse(price) > 1 || first_cheapest)
+                        {
+                            c_qty = quantity;
+                            first_cheapest = false;
+                            cheapest_String = price;
+                            cheapest = float.Parse(price);
+                        }
+                        buyList.Add(toAdd);
+                    }
+                    else
+                    {
+                        if (expensive > float.Parse(price) && float.Parse(price) < 100000000 || first_expensive)
+                        {
+                            e_qty = quantity;
+                            first_expensive = false;
+                            expensive_String = price;
+                            expensive = float.Parse(price);
+                        }
+                        sellList.Add(toAdd);
+                    }
+
+                    i++;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+            float profit = cheapest - expensive;
+            String[] sellBuy = { "They sell from(qty " + e_qty + "): " + expensive_String, "They buy from(qty " + c_qty + "): " + cheapest_String };
+            result.Add(sellBuy);
+            result.Add(profit);
+            result.Add(buyList);
+            result.Add(sellList);
+            return result;
+        }
+
+        private ArrayList readItemList(String data)
+        {
+            ArrayList result = new ArrayList();
+            String[] array = data.Split('\n');
+            /*
+             * 0 : api link
+             * 1 : numeric id
+             * 2 : string id
+             */
+            for (int i=0; i<array.Length ;)
+            {
+                String tmp = array[i];
+                if (tmp.Contains("id"))
+                {
+                    // API = i-1
+                    String apiLink = array[i - 1].Split(':')[0].Trim().Trim('"');
+                    // numeric id = i
+                    String numericId = array[i].Split(':')[1].Trim().Trim(',');
+                    // string id = i+1
+                    String stringId = array[i + 1].Split(':')[1].Trim().Trim('"');
+
+                    String[] toAdd = { apiLink, numericId, stringId };
+
+                    String URL = API_URL_BASE + apiLink;
+
+                    ArrayList tradeValues = readTradeValues(getData(URL), apiLink);
+                    ArrayList add = new ArrayList();
+
+                    String engName = "";
+                    foreach(String[] s in itemEnglish)
+                    {
+                        if(s[0].Contains(stringId))
+                        {
+                            engName = s[1];
+                        }
+                    }
+
+                    add.Add(engName);
+                    add.Add(tradeValues);
+                    add.Add(toAdd);
+                    if ((float)tradeValues[1] > 0 && (float)tradeValues[1] != -1)
+                        try
+                        {
+                            invoke((float)tradeValues[1], (string[])tradeValues[0], apiLink);
+                        }
+                        catch(Exception)
+                        {
+                            return result;
+                        }
+                    
+                    result.Add(add);
+
+                    i += 1;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+            try
+            {
+                String[] n = {"", ""};
+                invoke(0, n, "Done");
+            }
+            catch (Exception)
+            {
+                return result;
+            }
+            return result;
+        }
+
+        private double dTime = 0;
+        private bool double_click = false;
+        private void itemListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            /*dTime = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+            
+            if (double_click)
+            {
+                String s = (String)itemListBox.SelectedItem;
+                s = s.Split('\t')[0].Trim();
+                Console.WriteLine(s);
+                System.Diagnostics.Process.Start("https://butt.boundless.mayumi.fi/#item=" + s);
+            }
+            else
+                new Thread(delegate() 
+                {
+                    while (dTime + 500 > DateTimeOffset.Now.ToUnixTimeMilliseconds())
+                    {
+                        double_click = true;
+                    }
+                }).Start();*/
+        }
+
+        private int oldRowIndex = -1;
+        private int oldColIndex = -1;
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            dTime = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+            int index = e.RowIndex;
+            int colIndex = e.ColumnIndex;
+            Console.WriteLine(colIndex);
+            if (index != -1)
+                Console.WriteLine(dataGridView1.Rows[index].Cells[0].Value.ToString());
+            if (index != -1 && oldRowIndex==index && oldColIndex == colIndex)
+            {
+                String s = dataGridView1.Rows[index].Cells[0].Value.ToString();
+                s = s.Split('\t')[0].Trim();
+                System.Diagnostics.Process.Start("https://butt.boundless.mayumi.fi/#item=" + s);
+            }
+            
+            oldRowIndex = index;
+            oldColIndex = colIndex;
         }
     }
 }
